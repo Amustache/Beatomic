d3e80ae0ff133d45230d8d001a1efd028b97ec3f
Stache <stache@stache.cat>
2025-03-22 22:58:40 +0100

    Add cursors

---
+++ cursors.lua
@@ -0,0 +1,11 @@
+---
+--- Created by stache.
+--- DateTime: 22.03.25 22:55
+---
+cursors = {
+    point = love.mouse.newCursor("img/cursors/hand_point.png", 0, 0),
+    open = love.mouse.newCursor("img/cursors/hand_open.png", 0, 0),
+    closed = love.mouse.newCursor("img/cursors/hand_closed.png", 0, 0),
+}
+
+return cursors
